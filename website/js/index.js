cd7b19d5c8520d71bbf2c9a3c977b5beedfd4481
2004Vishnu <vishnureddykoppad@gmail.com>
2026-02-06 07:14:26 +0530

    added textbox from which text can be copied to clipboard

--- website/js/index.js
+++ website/js/index.js
@@ -8,3 +8,21 @@ let img1=document.getElementById("myimg");
 img1.addEventListener("load",function(){
      document.getElementById("img").innerHTML = "Page is fully loaded!";
     })
+
+    function copyText() {
+      const text = document.getElementById("content").value;
+      window.navigator.clipboard.writeText(text);
+    }
+      
+  function pasteAppend() {
+      window.navigator.clipboard.readText()
+        .then(text => {
+          document.getElementById("content1").value += text;
+        });
+    }
+
+    function pasteReplace() {
+      window.navigator.clipboard.readText()
+        .then(text => {
+          document.getElementById("content1").value = text;
+        });}
